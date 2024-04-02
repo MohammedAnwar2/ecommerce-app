3c6a979cdf50ea710386ad87e8d12c89f4b1eecd
mohammed anwar <moanbm123@gmail.com>
2024-04-02 13:03:38 +0530

    Add custom card, notification, and search product widgets
    Added custom card, custom notification, and search product widgets to the home screen for improved UI and functionality.

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,5 +1,10 @@
 import 'package:ecommerce/controller/home_controller.dart';
+import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
+import 'package:ecommerce/view/widget/home/custom_card.dart';
+import 'package:ecommerce/view/widget/home/custom_notification.dart';
+import 'package:ecommerce/view/widget/home/search_product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
@@ -8,27 +13,27 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeControllerImp controller = Get.put(HomeControllerImp());
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          TextButton(
-              onPressed: () {
-                controller.logoutApp();
-              },
-              child: Text("Logout"))
+        body: SafeArea(
+            child: Container(
+      child: ListView(
+        children: [
+          Container(
+            margin: EdgeInsetsDirectional.only(top: verticalSized(10)),
+            padding:
+                EdgeInsetsDirectional.symmetric(horizontal: horizontalSize(16)),
+            child: Row(
+              children: [
+                const Expanded(
+                  child: SearchProduct(),
+                ),
+                horizontalSizedBox(10),
+                const CustomNotification()
+              ],
+            ),
+          ),
+          CustomCard()
         ],
-        title: const Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(controller.name),
-            Text(controller.email),
-            Text(controller.phone),
-            Text(controller.id.toString()),
-          ],
-        ),
-      ),
-    );
+    )));
   }
 }
