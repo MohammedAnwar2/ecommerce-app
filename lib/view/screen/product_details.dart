3b7a69955b266ee17e00b452e332b2e49829bfa4
mohammed anwar <moanbm123@gmail.com>
2024-04-08 03:25:13 +0530

    Update import statement for custom add to cart button navigation widget
    Replace the import statement for the custom add to cart button navigation
    widget in the product details screen to match the actual file name.

--- lib/view/screen/product_details.dart
+++ lib/view/screen/product_details.dart
@@ -5,7 +5,7 @@ import 'package:ecommerce/view/widget/product_details/change_color.dart';
 import 'package:ecommerce/view/widget/product_details/details_text_title.dart';
 import 'package:ecommerce/view/widget/product_details/image_veiw.dart';
 import 'package:ecommerce/view/widget/product_details/product_count.dart';
-import 'package:ecommerce/view/widget/product_details/xxx.dart';
+import 'package:ecommerce/view/widget/product_details/custom_add_to_cart_burrom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
