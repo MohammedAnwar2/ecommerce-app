d16fd10f17248fcf3f4f8e67253adbae48cfc684
mohammed anwar <moanbm123@gmail.com>
2024-04-08 03:24:38 +0530

    Add custom add to cart button widget
    Create a new widget for a custom add to cart button with specific styling and functionality.

---
+++ lib/view/widget/product_details/custom_add_to_cart_burrom_navigation.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce/core/constant/app_color.dart';
+import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
+import 'package:flutter/material.dart';
+
+class CustomAddToCartBurromNavigation extends StatelessWidget {
+  const CustomAddToCartBurromNavigation({
+    super.key,
+    required this.onPressed,
+  });
+  final void Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(horizontalSize(10)),
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(8.0), // Adjust the radius value as needed
+        ),
+        color: AppColor.primaryColor,
+        onPressed: onPressed,
+        child: Text(
+          "Add To Cart",
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: AppColor.white),
+        ),
+      ),
+    );
+  }
+}
