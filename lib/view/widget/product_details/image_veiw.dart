7a71afb467d6207364ce4ccbda0f38099b5b70d6
mohammed anwar <moanbm123@gmail.com>
2024-04-08 03:23:01 +0530

    Add ImageVeiw widget for displaying product image in ProductDetails screen

---
+++ lib/view/widget/product_details/image_veiw.dart
@@ -0,0 +1,43 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecommerce/controller/product_details_controller.dart';
+import 'package:ecommerce/core/constant/app_color.dart';
+import 'package:ecommerce/core/constant/app_link.dart';
+import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ImageVeiw extends GetView<ProductDetailsControllerImp> {
+  const ImageVeiw({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: verticalSized(200),
+          decoration: BoxDecoration(
+            color: AppColor.primaryColor,
+            borderRadius: BorderRadius.circular(24),
+          ),
+        ),
+        Positioned(
+          top: verticalSized(50),
+          right: Get.width / 6,
+          left: Get.width / 6,
+          child: Hero(
+            tag: "${controller.itemModel.itemsId}",
+            child: CachedNetworkImage(
+              imageUrl:
+                  "${AppLink.imageItem}/${controller.itemModel.itemsImage!}",
+              height: verticalSized(230),
+              // fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
