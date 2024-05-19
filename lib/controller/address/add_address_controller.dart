25680c297383017d8f01fc1f2db93b889d1c6054
mohammed anwar <moanbm123@gmail.com>
2024-05-20 00:30:51 +0530

    Add AddAddressController and update AddAddress screen
    - Added AddAddressController to handle logic for adding an address
    - Updated AddAddress screen to use GetX for state management and to initialize AddAddressController.

---
+++ lib/controller/address/add_address_controller.dart
@@ -0,0 +1,6 @@
+import 'package:ecommerce/routes/route_app.dart';
+import 'package:get/get.dart';
+
+abstract class AddAddressController extends GetxController {}
+
+class AddAddressControllerIma extends AddAddressController {}
--- lib/view/screen/address/add.dart
+++ lib/view/screen/address/add.dart
@@ -1,16 +1,22 @@
+import 'package:ecommerce/controller/address/add_address_controller.dart';
 import 'package:ecommerce/core/constant/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AddAddress extends StatelessWidget {
   const AddAddress({super.key});
 
   @override
   Widget build(BuildContext context) {
+    AddAddressControllerIma addAddress = Get.put(AddAddressControllerIma());
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Add Address",
-          style: Theme.of(context).textTheme.displayLarge!.copyWith(color: AppColor.white),
+          style: Theme.of(context)
+              .textTheme
+              .displayLarge!
+              .copyWith(color: AppColor.white),
         ),
         iconTheme: const IconThemeData(color: AppColor.white),
       ),
