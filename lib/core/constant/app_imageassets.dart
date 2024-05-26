bff63d9922d2d814264a9d20d8f637ff4a15a488
mohammed anwar <moanbm123@gmail.com>
2024-05-27 03:26:12 +0530

    Add deliveryman and deliverycar images to AppImages constant class

--- lib/core/constant/app_imageassets.dart
+++ lib/core/constant/app_imageassets.dart
@@ -1,12 +1,16 @@
 class AppImages {
   static const String imagesRoute = "assets/images";
   static const String lottieRoute = "assets/lottie";
+  //*=================== image ===================
   static const String logoAuth = "$imagesRoute/logo.png";
   static const String onBoardingImageOne = "$imagesRoute/one.PNG";
   static const String onBoardingImageTwo = "$imagesRoute/two.PNG";
   static const String onBoardingImageThree = "$imagesRoute/three.PNG";
   static const String sale = "$imagesRoute/sale.png";
   static const String profile = "$imagesRoute/profile.png";
+  static const String deliveryman = "$imagesRoute/deliveryman.svg";
+  static const String deliverycar = "$imagesRoute/deliverycar.svg";
+  //*=================== lotti ===================
   static const String loading = "$lottieRoute/loading.json";
   static const String offline = "$lottieRoute/offline.json";
   static const String server = "$lottieRoute/server.json";
