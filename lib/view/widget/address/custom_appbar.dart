fb918a309f87f956ce18bed884a7a8faf095587e
mohammed anwar <moanbm123@gmail.com>
2024-05-26 23:21:57 +0530

    Comment out CustomAppBar widget in address screen
    The CustomAppBar widget in the address screen has been commented out to remove the dependency on AppColor and Flutter Material libraries.

--- lib/view/widget/address/custom_appbar.dart
+++ lib/view/widget/address/custom_appbar.dart
@@ -1,25 +1,25 @@
-import 'package:ecommerce/core/constant/app_color.dart';
-import 'package:flutter/material.dart';
+// import 'package:ecommerce/core/constant/app_color.dart';
+// import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
+// class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   final String title;
 
-  const CustomAppBar({required this.title});
+//   const CustomAppBar({required this.title});
 
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        title,
-        style: Theme.of(context)
-            .textTheme
-            .displayLarge!
-            .copyWith(color: AppColor.white),
-      ),
-      iconTheme: const IconThemeData(color: AppColor.white),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       title: Text(
+//         title,
+//         style: Theme.of(context)
+//             .textTheme
+//             .displayLarge!
+//             .copyWith(color: AppColor.white),
+//       ),
+//       iconTheme: const IconThemeData(color: AppColor.white),
+//     );
+//   }
 
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-}
+//   @override
+//   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+// }
