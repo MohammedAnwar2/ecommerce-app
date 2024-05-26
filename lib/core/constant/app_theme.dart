2d42fa6d1955c62aae7f34cc5661caffaaafc466
mohammed anwar <moanbm123@gmail.com>
2024-05-26 23:22:23 +0530

    Refactor app theme styles for better readability and consistency

--- lib/core/constant/app_theme.dart
+++ lib/core/constant/app_theme.dart
@@ -3,8 +3,21 @@ import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
 import 'package:flutter/material.dart';
 
 ThemeData themeEnglish = ThemeData(
-  appBarTheme: const AppBarTheme(backgroundColor: AppColor.primaryColor),
-  floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: AppColor.primaryColor),
+  appBarTheme: AppBarTheme(
+    titleTextStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontFamily: "PlayfairDisplay",
+        fontSize: fontSize(24),
+        color: AppColor.primaryColor),
+    elevation: 0,
+    centerTitle: true,
+    backgroundColor: AppColor.white,
+    iconTheme:
+        IconThemeData(color: AppColor.primaryColor, size: horizontalSize(25)),
+  ),
+  scaffoldBackgroundColor: AppColor.white,
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColor.primaryColor),
   fontFamily: "PlayfairDisplay",
   textTheme: TextTheme(
     bodyLarge: TextStyle(
@@ -13,10 +26,18 @@ ThemeData themeEnglish = ThemeData(
         // fontSize: fontSize(14),
         height: 2,
         fontWeight: FontWeight.bold),
-    bodyMedium: TextStyle(color: AppColor.grey, fontSize: fontSize(14), height: 2),
-    bodySmall: TextStyle(color: AppColor.grey, fontSize: fontSize(12), height: 2),
-    displaySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize(16), color: AppColor.white),
-    displayLarge: TextStyle(color: AppColor.black, fontSize: fontSize(20), fontWeight: FontWeight.bold),
+    bodyMedium:
+        TextStyle(color: AppColor.grey, fontSize: fontSize(14), height: 2),
+    bodySmall:
+        TextStyle(color: AppColor.grey, fontSize: fontSize(12), height: 2),
+    displaySmall: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: fontSize(16),
+        color: AppColor.white),
+    displayLarge: TextStyle(
+        color: AppColor.black,
+        fontSize: fontSize(20),
+        fontWeight: FontWeight.bold),
     displayMedium: TextStyle(
       color: AppColor.black,
       fontSize: fontSize(26),
@@ -26,15 +47,40 @@ ThemeData themeEnglish = ThemeData(
 );
 
 ThemeData themeArabic = ThemeData(
-  appBarTheme: const AppBarTheme(backgroundColor: AppColor.primaryColor),
-  floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: AppColor.primaryColor),
+  appBarTheme: AppBarTheme(
+    titleTextStyle: TextStyle(
+        fontFamily: "Cairo",
+        fontWeight: FontWeight.bold,
+        fontSize: fontSize(24),
+        color: AppColor.primaryColor),
+    elevation: 0,
+    centerTitle: true,
+    backgroundColor: AppColor.white,
+    iconTheme:
+        IconThemeData(color: AppColor.primaryColor, size: horizontalSize(25)),
+  ),
+  scaffoldBackgroundColor: AppColor.white,
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColor.primaryColor),
   fontFamily: "Cairo",
   textTheme: TextTheme(
-    bodyLarge: TextStyle(color: AppColor.grey, fontSize: fontSize(14), height: 2, fontWeight: FontWeight.bold),
-    displaySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize(16), color: AppColor.white),
-    bodyMedium: TextStyle(color: AppColor.grey, fontSize: fontSize(14), height: 2),
-    bodySmall: TextStyle(color: AppColor.grey, fontSize: fontSize(12), height: 2),
-    displayLarge: TextStyle(color: AppColor.black, fontSize: fontSize(20), fontWeight: FontWeight.bold),
+    bodyLarge: TextStyle(
+        color: AppColor.grey,
+        fontSize: fontSize(14),
+        height: 2,
+        fontWeight: FontWeight.bold),
+    displaySmall: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: fontSize(16),
+        color: AppColor.white),
+    bodyMedium:
+        TextStyle(color: AppColor.grey, fontSize: fontSize(14), height: 2),
+    bodySmall:
+        TextStyle(color: AppColor.grey, fontSize: fontSize(12), height: 2),
+    displayLarge: TextStyle(
+        color: AppColor.black,
+        fontSize: fontSize(20),
+        fontWeight: FontWeight.bold),
     displayMedium: TextStyle(
       color: AppColor.black,
       fontSize: fontSize(26),
