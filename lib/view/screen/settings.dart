2029e464df71ff903827074980a760e369c1c1bc
mohammed anwar <moanbm123@gmail.com>
2024-05-31 23:22:13 +0530

    Add CustomFadeTransitionAnimation to Settings screen
    This commit adds a CustomFadeTransitionAnimation widget to the Settings screen, which provides a fade-in effect when the screen is loaded. The animation time is set to 600 milliseconds. No functional changes were made to the existing code.

--- lib/view/screen/settings.dart
+++ lib/view/screen/settings.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:ecommerce/controller/settings_controller.dart';
+import 'package:ecommerce/core/class/animation.dart';
 import 'package:ecommerce/core/constant/app_color.dart';
 import 'package:ecommerce/core/constant/app_imageassets.dart';
 import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
@@ -41,53 +42,56 @@ class Settings extends StatelessWidget {
           ],
         ),
         verticalSizedBox(50),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            color: AppColor.white,
-            child: Column(
-              children: [
-                ListTile(
-                  title: const Text("Disable Notification"),
-                  trailing: Switch(value: true, onChanged: (val) {}),
-                ),
-                ListTile(
-                  onTap: () {
-                    controller.goToAddress();
-                  },
-                  title: const Text("Address"),
-                  trailing: const Icon(Icons.location_on_outlined),
-                ),
-                ListTile(
-                  onTap: () {
-                    controller.goToOrdersPending();
-                  },
-                  title: const Text("Orders Pending"),
-                  trailing: const Icon(Icons.pending_actions_sharp),
-                ),
-                ListTile(
-                  onTap: () {
-                    controller.goToOrdersArchive();
-                  },
-                  title: const Text("Orders Archive"),
-                  trailing: const Icon(Icons.archive_outlined),
-                ),
-                const ListTile(
-                  title: Text("About us"),
-                  trailing: Icon(Icons.help_outline_sharp),
-                ),
-                const ListTile(
-                  title: Text("Contact us"),
-                  trailing: Icon(Icons.phone_callback_outlined),
-                ),
-                ListTile(
-                  onTap: () {
-                    controller.logout();
-                  },
-                  title: const Text("Logout"),
-                  trailing: const Icon(Icons.logout_outlined),
-                ),
-              ],
+        CustomFadeTransitionAnimation(
+          time: 600,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: AppColor.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text("Disable Notification"),
+                    trailing: Switch(value: true, onChanged: (val) {}),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      controller.goToAddress();
+                    },
+                    title: const Text("Address"),
+                    trailing: const Icon(Icons.location_on_outlined),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      controller.goToOrdersPending();
+                    },
+                    title: const Text("Orders Pending"),
+                    trailing: const Icon(Icons.pending_actions_sharp),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      controller.goToOrdersArchive();
+                    },
+                    title: const Text("Orders Archive"),
+                    trailing: const Icon(Icons.archive_outlined),
+                  ),
+                  const ListTile(
+                    title: Text("About us"),
+                    trailing: Icon(Icons.help_outline_sharp),
+                  ),
+                  const ListTile(
+                    title: Text("Contact us"),
+                    trailing: Icon(Icons.phone_callback_outlined),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      controller.logout();
+                    },
+                    title: const Text("Logout"),
+                    trailing: const Icon(Icons.logout_outlined),
+                  ),
+                ],
+              ),
             ),
           ),
         )
