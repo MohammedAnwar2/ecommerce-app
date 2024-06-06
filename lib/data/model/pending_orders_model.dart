e8144f108ffadf4a3f278b30db4350dd5c324259
mohammed anwar <moanbm123@gmail.com>
2024-06-06 09:22:31 +0530

    add new attributes for PendingOrdersModel

--- lib/data/model/pending_orders_model.dart
+++ lib/data/model/pending_orders_model.dart
@@ -10,6 +10,13 @@ class PendingOrdersModel {
   int? ordersCoupon;
   String? ordersDatetime;
   int? ordersStatus;
+  int? addressId;
+  int? addressUsersId;
+  String? addressName;
+  String? addressCity;
+  String? addressStreet;
+  double? addressLat;
+  double? addressLong;
 
   PendingOrdersModel({
     this.ordersId,
@@ -23,6 +30,13 @@ class PendingOrdersModel {
     this.ordersCoupon,
     this.ordersDatetime,
     this.ordersStatus,
+    this.addressId,
+    this.addressUsersId,
+    this.addressName,
+    this.addressCity,
+    this.addressStreet,
+    this.addressLat,
+    this.addressLong,
   });
 
   factory PendingOrdersModel.fromJson(Map<String, dynamic> json) =>
@@ -38,6 +52,13 @@ class PendingOrdersModel {
         ordersCoupon: json['orders_coupon'] as int?,
         ordersDatetime: json['orders_datetime'] as String?,
         ordersStatus: json['orders_status'] as int?,
+        addressId: json['address_id'] as int?,
+        addressUsersId: json['address_usersId'] as int?,
+        addressName: json['address_name'] as String?,
+        addressCity: json['address_city'] as String?,
+        addressStreet: json['address_street'] as String?,
+        addressLat: (json['address_lat'] as num?)?.toDouble(),
+        addressLong: (json['address_long'] as num?)?.toDouble(),
       );
 
   Map<String, dynamic> toJson() => {
@@ -52,5 +73,12 @@ class PendingOrdersModel {
         'orders_coupon': ordersCoupon,
         'orders_datetime': ordersDatetime,
         'orders_status': ordersStatus,
+        'address_id': addressId,
+        'address_usersId': addressUsersId,
+        'address_name': addressName,
+        'address_city': addressCity,
+        'address_street': addressStreet,
+        'address_lat': addressLat,
+        'address_long': addressLong,
       };
 }
