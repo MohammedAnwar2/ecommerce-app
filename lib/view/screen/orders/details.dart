40643f10b77a766f2698471c9a50483afcde2983
mohammed anwar <moanbm123@gmail.com>
2024-06-06 21:32:46 +0530

    add GetBuilder and ...List.generate loop

--- lib/view/screen/orders/details.dart
+++ lib/view/screen/orders/details.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/controller/orders/details_controller.dart';
+import 'package:ecommerce/core/class/handling_data_veiw.dart';
 import 'package:ecommerce/core/constant/app_color.dart';
 import 'package:ecommerce/core/shared/horizontal_and_vertical_size.dart';
 import 'package:flutter/material.dart';
@@ -11,123 +12,165 @@ class OrderDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    OrderDetailsControllerImp controller = Get.put(OrderDetailsControllerImp());
+    Get.put(OrderDetailsControllerImp());
     return Scaffold(
       appBar: AppBar(title: const Text("Order Details")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            Card(
-              color: AppColor.white,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Table(
+        child: GetBuilder<OrderDetailsControllerImp>(builder: (controller) {
+          return HandlingDataView(
+            statusRequest: controller.statusRequest,
+            widget: ListView(
+              children: [
+                Card(
+                  color: AppColor.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        TableRow(children: [
-                          Center(
-                            child: Text("Item",
-                                style: TextStyle(
-                                    color: AppColor.secondaryColor,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: fontSize(15))),
-                          ),
-                          Center(
-                            child: Text("QTY",
-                                style: TextStyle(
-                                    color: AppColor.secondaryColor,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: fontSize(15))),
-                          ),
-                          Center(
-                            child: Text("Price",
-                                style: TextStyle(
-                                    color: AppColor.secondaryColor,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: fontSize(15))),
-                          ),
-                        ]),
-                        const TableRow(children: [
-                          Center(
-                              child: Text(
-                            "Macbook m1",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )),
-                          Center(
-                              child: Text("1",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold))),
-                          Center(
-                              child: Text("1200",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold))),
-                        ]),
-                        const TableRow(children: [
-                          Center(
-                              child: Text("S22 Ultra",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold))),
-                          Center(
-                              child: Text("2",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold))),
-                          Center(
-                              child: Text("1100",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold))),
-                        ]),
+                        Table(
+                          children: [
+                            TableRow(children: [
+                              Center(
+                                child: Text("Item",
+                                    style: TextStyle(
+                                        color: AppColor.secondaryColor,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: fontSize(15))),
+                              ),
+                              Center(
+                                child: Text("QTY",
+                                    style: TextStyle(
+                                        color: AppColor.secondaryColor,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: fontSize(15))),
+                              ),
+                              Center(
+                                child: Text("Price",
+                                    style: TextStyle(
+                                        color: AppColor.secondaryColor,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: fontSize(15))),
+                              ),
+                            ]),
+                            ...List.generate(
+                                controller.ordersDetailsList.length,
+                                (i) => TableRow(children: [
+                                      Center(
+                                          child: Text(
+                                        controller
+                                            .ordersDetailsList[i].itemsName
+                                            .toString(),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      )),
+                                      Center(
+                                          child: Text(
+                                              controller.ordersDetailsList[i]
+                                                  .itemscount
+                                                  .toString(),
+                                              style: const TextStyle(
+                                                  fontWeight:
+                                                      FontWeight.bold))),
+                                      Center(
+                                          child: Text(
+                                              controller.ordersDetailsList[i]
+                                                  .itemsPrice
+                                                  .toString(),
+                                              style: const TextStyle(
+                                                  fontWeight:
+                                                      FontWeight.bold))),
+                                    ])),
+                          ],
+                        ),
+                        //Divider(),
+                        CustomPriceCard(
+                            text: "Delivery Price",
+                            price: controller.pendingOrders.ordersPricedelivery!
+                                .toString()),
+                        CustomPriceCard(
+                            text: "Total Price",
+                            price: controller.pendingOrders.ordersTotalprice!
+                                .toString()),
                       ],
                     ),
-                    Text(
-                      "Price : 2400\$",
-                      style: TextStyle(
-                          color: AppColor.secondaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: fontSize(15)),
-                    )
-                  ],
+                  ),
                 ),
-              ),
+                if (controller.pendingOrders.ordersType == 0)
+                  Card(
+                    color: AppColor.white,
+                    child: ListTile(
+                      title: Text(
+                        "Shipping Address",
+                        style: TextStyle(
+                            color: AppColor.secondaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: fontSize(15)),
+                      ),
+                      subtitle: Text(
+                        "${controller.pendingOrders.addressCity!} - ${controller.pendingOrders.addressStreet!} - ${controller.pendingOrders.addressName!}",
+                        style: TextStyle(
+                            color: AppColor.grey500,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                if (controller.pendingOrders.ordersType == 0)
+                  Card(
+                    color: AppColor.white,
+                    child: Container(
+                      padding: EdgeInsets.all(horizontalSize(10)),
+                      height: horizontalSize(300),
+                      width: double.infinity,
+                      child: GoogleMap(
+                        markers: controller.markers,
+                        mapType: MapType.normal,
+                        initialCameraPosition: controller.cameraPosition,
+                        onMapCreated:
+                            (GoogleMapController googleMapController) {
+                          controller.completorController
+                              .complete(googleMapController);
+                        },
+                      ),
+                    ),
+                  )
+              ],
             ),
-            Card(
-              color: AppColor.white,
-              child: ListTile(
-                title: Text(
-                  "Shipping Address",
-                  style: TextStyle(
-                      color: AppColor.secondaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: fontSize(15)),
-                ),
-                subtitle: Text(
-                  "Damascus Street one",
-                  style: TextStyle(
-                      color: AppColor.grey500, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            Card(
-              color: AppColor.white,
-              child: Container(
-                padding: EdgeInsets.all(horizontalSize(10)),
-                height: horizontalSize(300),
-                width: double.infinity,
-                child: GoogleMap(
-                  markers: controller.markers,
-                  mapType: MapType.normal,
-                  initialCameraPosition: controller.cameraPosition,
-                  onMapCreated: (GoogleMapController googleMapController) {
-                    controller.completorController
-                        .complete(googleMapController);
-                  },
-                ),
-              ),
-            )
-          ],
-        ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class CustomPriceCard extends StatelessWidget {
+  const CustomPriceCard({super.key, required this.price, required this.text});
+  final String price;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: horizontalSize(36)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+                color: AppColor.secondaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: fontSize(15)),
+          ),
+          Text(
+            price,
+            style: TextStyle(
+                color: AppColor.secondaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: fontSize(15)),
+          ),
+        ],
       ),
     );
   }
