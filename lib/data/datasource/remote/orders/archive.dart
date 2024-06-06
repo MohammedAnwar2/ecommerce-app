96b78d56059bcbe29e9e96199d860050f52c8947
mohammed anwar <moanbm123@gmail.com>
2024-06-06 21:58:47 +0530

    create the archive remote data

--- lib/data/datasource/remote/orders/archive.dart
+++ lib/data/datasource/remote/orders/archive.dart
@@ -1,1 +1,13 @@
+import 'package:ecommerce/core/class/crud.dart';
+import 'package:ecommerce/core/constant/app_link.dart';
 
+class ArchiveData {
+  Crud crud;
+  ArchiveData(this.crud);
+
+  archiveOrder(int userId) async {
+    var response =
+        await crud.postData(AppLink.archive, {"userid": userId.toString()});
+    return response.fold((l) => l, (r) => r);
+  }
+}
